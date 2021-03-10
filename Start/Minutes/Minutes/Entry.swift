d53d394a0f4fb4ef919c2e1814435a41e0172056
MiraDzoba <miradzoba21@gmail.com>
2021-03-10 19:58:23 +0200

    testing hello

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class Entry: Codable
 {
-    var title:   String = "Hello"
+    var title:   String = "Hello Mira Dzoba"
     var content: String = "Thereeeee"
     
     var id: String = UUID().uuidString
